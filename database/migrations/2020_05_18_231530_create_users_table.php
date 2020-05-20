3e16d0a8acce6a254c6e5c689f3c9311eed5c343
Roberto Arruda <robertoadearruda@gmail.com>
2020-05-20 19:41:12 -0300

    [Fix] User size fields

--- database/migrations/2020_05_18_231530_create_users_table.php
+++ database/migrations/2020_05_18_231530_create_users_table.php
@@ -14,18 +14,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('firm_id')->default(0);
-            $table->string('email')->unique()->default('');
-            $table->string('password')->default('');
-            $table->string('key')->default('');
-            $table->string('first_name')->default('');
-            $table->string('last_name')->default('');
-            $table->string('phone_number')->nullable();
-            $table->string('extension')->nullable();
-            $table->string('mobile_number')->nullable();
+            $table->string('email', 64)->unique()->default('');
+            $table->string('password', 64)->default('');
+            $table->string('key', 32)->default('');
+            $table->string('first_name', 64)->default('');
+            $table->string('last_name', 64)->default('');
+            $table->string('phone_number', 16)->nullable();
+            $table->string('extension', 16)->nullable();
+            $table->string('mobile_number', 16)->nullable();
             $table->text('summary')->nullable();
             $table->enum('status', ['active', 'disabled'])->default('active');
-            $table->string('profile_image')->nullable();
-            $table->string('activation_code')->nullable();
+            $table->integer('profile_image')->nullable();
+            $table->string('activation_code', 64)->nullable();
             $table->dateTime('activation_timestamp')->nullable();
             $table->boolean('invited')->nullable();
             $table->rememberToken();
