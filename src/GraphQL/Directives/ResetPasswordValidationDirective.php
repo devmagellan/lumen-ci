92405445306fb155cc91eedcd958a4980a6ff0b0
Roberto Arruda <robertoadearruda@gmail.com>
2020-05-27 23:17:25 -0300

    Implementing GraphQL

---
+++ src/GraphQL/Directives/ResetPasswordValidationDirective.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WGT\GraphQL\Directives;
+
+use Nuwave\Lighthouse\Schema\Directives\ValidationDirective;
+
+class ResetPasswordValidationDirective extends ValidationDirective
+{
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [];
+    }
+}
