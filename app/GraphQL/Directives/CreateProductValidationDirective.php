9f77d296150f25f63b53a7f3ee2d97f002f5540a
Roberto Arruda <robertoadearruda@gmail.com>
2020-07-14 22:57:21 -0300

    [Feature] Product

---
+++ app/GraphQL/Directives/CreateProductValidationDirective.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WGT\GraphQL\Directives;
+
+use Illuminate\Validation\Rule;
+use Nuwave\Lighthouse\Schema\Directives\ValidationDirective;
+
+class CreateProductValidationDirective extends ValidationDirective
+{
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:64'],
+            'description' => ['string'],
+            'type' => ['string', Rule::in(['item', 'package'])],
+            'price' => ['numeric'],
+            'association_discount' => ['numeric'],
+            'retail_price' => ['numeric'],
+            'report_price' => ['numeric'],
+            'cost' => ['numeric'],
+            'visible' => ['string', Rule::in(['public', 'private', 'member'])],
+            'status' => ['string', Rule::in(['display', 'sale', 'sold', 'pending'])],
+            'active' => ['boolean'],
+            'vendor_sku' => ['string'],
+            'anonymous' => ['boolean'],
+            'matching' => ['integer'],
+            'association_product' => ['string'],
+            'matching_sku' => ['string'],
+            'currency_id' => ['required', 'integer'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [];
+    }
+}
